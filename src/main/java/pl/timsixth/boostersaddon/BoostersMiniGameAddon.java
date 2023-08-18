70c91c893a890d4df3dac6b5475f22f358fda38c
timsixth <timsixth@op.pl>
2023-08-18 12:55:30 +0200

    Initial commit

---
+++ src/main/java/pl/timsixth/boostersaddon/BoostersMiniGameAddon.java
@@ -0,0 +1,12 @@
+package pl.timsixth.boostersaddon;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BoostersMiniGameAddon extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        Bukkit.getLogger().info("testing addon");
+    }
+}
